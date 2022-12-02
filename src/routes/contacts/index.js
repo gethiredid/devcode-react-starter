d648e895a6e541a37181f52b42455d96211f46b4
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-12-02 17:32:05 +0700

    add level 2 test

--- src/routes/contacts/index.js
+++ src/routes/contacts/index.js
@@ -1,40 +1,28 @@
-import { useEffect, useState } from "react";
 import InputContactForm from "../../components/InputContactForm";
-import ContactItem from "../../components/ContactItem";
 import "../../assets/styles/style.css";
-import { getAllContactsData } from "../../services";
 
 const ContactManager = () => {
-  const [contactsData, setContactsData] = useState([]);
-  const [selectedContact, setSelectedContact] = useState({});
+  // Uncomment code below
+  // const [contactsData, setContactsData] = useState([]);
 
-  const handleGetContactsData = async () => {
-    const res = await getAllContactsData();
-    setContactsData(res?.data?.data);
-  };
+  // Uncomment code below
+  // const handleGetContactsData = async () => {
+  //   const res = await getAllContactsData();
+  //   setContactsData(res?.data?.data);
+  // };
 
-  const handleSetSelectedContact = (id, name, phone, email) => {
-    setSelectedContact({
-      id,
-      fullName: name,
-      phoneNumber: phone,
-      email,
-    });
-  };
-
-  useEffect(() => {
-    handleGetContactsData();
-  }, []);
+  // Uncomment code below
+  // useEffect(() => {
+  //   handleGetContactsData();
+  // }, []);
 
   return (
     <div className="home">
       <div className="container">
-        <InputContactForm
-          handleGetContacts={handleGetContactsData}
-          selectedContact={selectedContact}
-        />
+        <InputContactForm />
         <div className="contact-list__container">
-          <>
+          {/* Uncomment code below */}
+          {/* <>
             {contactsData && contactsData?.length > 0
               ? contactsData.map((contact, key) => (
                   <ContactItem
@@ -43,12 +31,10 @@ const ContactManager = () => {
                     full_name={contact?.full_name}
                     phone_number={contact?.phone_number}
                     email={contact?.email}
-                    handleGetContacts={handleGetContactsData}
-                    handleSetSelected={handleSetSelectedContact}
                   />
                 ))
               : ""}
-          </>
+          </> */}
         </div>
       </div>
     </div>
