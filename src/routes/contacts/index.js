0dedbeebc46859557cf27a726abe0750495da3bb
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-12-02 20:27:50 +0700

    enable fetch contact data function

--- src/routes/contacts/index.js
+++ src/routes/contacts/index.js
@@ -1,28 +1,27 @@
 import InputContactForm from "../../components/InputContactForm";
+import ContactItem from "../../components/ContactItem";
 import "../../assets/styles/style.css";
+import { useEffect, useState } from "react";
+import { getAllContactsData } from "../../services";
 
 const ContactManager = () => {
-  // Uncomment code below
-  // const [contactsData, setContactsData] = useState([]);
+  const [contactsData, setContactsData] = useState([]);
 
-  // Uncomment code below
-  // const handleGetContactsData = async () => {
-  //   const res = await getAllContactsData();
-  //   setContactsData(res?.data?.data);
-  // };
+  const handleGetContactsData = async () => {
+    const res = await getAllContactsData();
+    setContactsData(res?.data?.data);
+  };
 
-  // Uncomment code below
-  // useEffect(() => {
-  //   handleGetContactsData();
-  // }, []);
+  useEffect(() => {
+    handleGetContactsData();
+  }, []);
 
   return (
     <div className="home">
       <div className="container">
         <InputContactForm />
         <div className="contact-list__container">
-          {/* Uncomment code below */}
-          {/* <>
+          <>
             {contactsData && contactsData?.length > 0
               ? contactsData.map((contact, key) => (
                   <ContactItem
@@ -34,7 +33,7 @@ const ContactManager = () => {
                   />
                 ))
               : ""}
-          </> */}
+          </>
         </div>
       </div>
     </div>
--- src/services/index.js
+++ src/services/index.js
@@ -1,12 +1,9 @@
-// Uncomment code below
-// import axios from "axios";
+import axios from "axios";
 
-// Uncomment code below
-// const BASE_URL = "https://api.contact-manager.project.skyshi.io";
+const BASE_URL = "https://api.contact-manager.project.skyshi.io";
 
-// Uncomment code below
-// export async function getAllContactsData() {
-//   return await axios.get(`${BASE_URL}/contacts`).catch((error) => {
-//     return error;
-//   });
-// }
+export async function getAllContactsData() {
+  return await axios.get(`${BASE_URL}/contacts`).catch((error) => {
+    return error;
+  });
+}
