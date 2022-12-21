759ed97478894c1148bfd6099ab2570ebcb9f2b0
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-12-21 10:15:09 +0700

    docs: add api docs and update step one on work instructions section

--- src/components/InputContactForm/index.js
+++ src/components/InputContactForm/index.js
@@ -8,11 +8,11 @@ const InputContactForm = (props) => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [email, setEmail] = useState("");
 
-  // Uncomment baris kode di bawah untuk membuat regex yang akan membantu memvalidasi format nomor telepon dan email
+  // TODO: Uncomment baris kode di bawah untuk membuat regex yang akan membantu memvalidasi format nomor telepon dan email
   // const regexPhoneNumber = /^[0-9]*$/;
   // const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-  // Challenge yang harus dikerjakan pada file ini
+  // TODO:
   // 1. Buat sebuah fungsi yang akan memvalidasi apakah format dari nomor telepon dan email yang dimasukkan sudah benar atau belum
   // 2. Jika format nomor telepon salah, maka tampilkan sebuah alert dengan isi pesan "Nomor telepon hanya dapat berupa angka."
   // 3. Jika format email salah, maka tampilkan sebuah alert dengan isi pesan "Format email tidak sesuai."
